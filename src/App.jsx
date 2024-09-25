ed736cb6d75a80bbd2c0284917ef4d49f6447362
saurabh-gupta-ccg <saurabhcser8787@gmail.com>
2024-09-25 22:21:04 +0530

    browser Router + Navbar initial code push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Component/Navbar";
+import Home from "./Component/Home";
+import Student from "./Component/Student";
+import Teacher from "./Component/Teacher";
+import Result from "./Component/Result";
+import Contact from "./Component/Contact";
+
+import "./App.css";
 
 function App() {
-
-
   return (
-    <>
-     
-    </>
-  )
+  
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/student" element={<Student />} />
+          <Route path="/teacher" element={<Teacher />} />
+          <Route path="/result" element={<Result />} />
+        </Routes>
+      </BrowserRouter>
+    
+  );
 }
 
-export default App
+export default App;
